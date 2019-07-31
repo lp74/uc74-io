961928cff4c954c6550922acfd5d92d70330be87
Luca Polverini <luca.polverini@it.abb.com>
2019-07-31 11:12:35 +0200

    feat(www): add www

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use('/', express.static(path.join(__dirname, 'www')));
 
 app.get('/v1/gate/open', (req, res) => res.send('Hello World!'))
 
